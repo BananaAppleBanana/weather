5044b3b44e5136a906abd5bcaf67f9082c75dce3
renqingyang <rq625230882@gmail.com>
2021-11-11 15:06:14 -0600

    homework

--- search/src/main/java/com/example/search/service/WeatherServiceImpl.java
+++ search/src/main/java/com/example/search/service/WeatherServiceImpl.java
@@ -41,3 +41,51 @@ public class WeatherServiceImpl implements WeatherService{
         return ans;
     }
 }
+
+
+/**
+ *  -> gateway -> eureka
+ *       |
+ *   weather-search -> hystrix(thread pool) -> 3rd party weather api
+ *
+ *
+ *  circuit breaker(hystrix)
+ * *  * *  * *  * *  * *  * *  * *  * *  * *  * *  * *  * *  * *  * *
+ *   weather-search service should get city id from detail service
+ *   and use multi-threading to query city's weather details
+ *
+ *   gateway
+ *     |
+ *  weather-service -> 3rd party api(id <-> weather)
+ *    |
+ *  detail-service -> 3rd party api (city <-> id)
+ *
+ *  failed situations:
+ *      1. 3rd party api timeout -> retry + hystrix
+ *      2. 3rd party api available time / rate limit
+ *      3. security verification
+ *  response
+ *      1. no id -> error / empty
+ *      2. large response -> pagination / file download (link / email)
+ *  performance
+ *      1. cache / db
+ *
+ *   gateway
+ *     |
+ *  weather-service -> cache(city - id - weather) (LFU)
+ *    |
+ *   DB (city - id - weather) <-> service <->  message queue  <-> scheduler <-> 3rd party api(city - id)
+ *                                                                  |
+ *                                                         update id - weather every 30 min
+ *                                                         update city - id relation once per day
+ *
+ *  homework :
+ *      deadline -> Wednesday midnight
+ *      1. update detail service
+ *          a. send request to 3rd party api -> get id by city
+ *      2. update search service
+ *          a. add ThreadPool
+ *          b. send request to detail service -> get id by city
+ *          c. use CompletableFuture send request to 3rd party api -> get weather by ids
+ *          d. add retry feature
+ */
